6e90a30dc04f9e160fe53fb4751b976f8f44819d
Ken Kier <KenK@Kens-MacBook-Pro.local>
2015-07-29 09:13:47 -0400

    Displays colors of hex values

--- app.js
+++ app.js
@@ -23,27 +23,33 @@
       dominant = document.createElement('div');
       dominant.className = "dominant";
       dominant.innerHTML = item.colors.dominant;
+      dominant.style.backgroundColor = dominant.innerHTML
+
       palette.appendChild(dominant);
 
       contrastingDominant = document.createElement('div');
       contrastingDominant.className = "contrastingDominant";
       contrastingDominant.innerHTML = item.colors.contrastingDominant;
+      contrastingDominant.style.backgroundColor = contrastingDominant.innerHTML
+
       palette.appendChild(contrastingDominant);
 
       subDominant = document.createElement('div');
       subDominant.className = "subDominant";
       subDominant.innerHTML = item.colors.subDominant;
+      subDominant.style.backgroundColor = subDominant.innerHTML
       palette.appendChild(subDominant);
 
       contrastingSubDominant = document.createElement('div');
       contrastingSubDominant.className = "contrastingSubDominant";
       contrastingSubDominant.innerHTML = item.colors.contrastingSubDominant;
+      contrastingSubDominant.style.backgroundColor = contrastingSubDominant.innerHTML
       palette.appendChild(contrastingSubDominant);
 
-      //pop
       pop = document.createElement('div');
       pop.className = "pop";
       pop.innerHTML = item.colors.pop;
+      pop.style.backgroundColor = pop.innerHTML
       palette.appendChild(pop);
 
 
